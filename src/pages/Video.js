3fec93b0ce6572fa3ea4be6767bddf0751b949db
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-29 08:31:50 +0600

    all warning clear done

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -9,10 +9,10 @@ export default function Video() {
         <>
             <Navbar />
 
-            <section class="pt-6 pb-20">
-                <div class="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
-                    <div class="grid grid-cols-3 gap-2 lg:gap-8">
-                        <div class="col-span-full w-full space-y-8 lg:col-span-2">
+            <section className="pt-6 pb-20">
+                <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
+                    <div className="grid grid-cols-3 gap-2 lg:gap-8">
+                        <div className="col-span-full w-full space-y-8 lg:col-span-2">
                             <VideoPlayer />
 
                             <VideoDescription />
