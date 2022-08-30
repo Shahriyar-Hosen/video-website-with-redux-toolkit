1f3e0dee45b8a3b0bd8a847563d1c426bfdeb17a
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-30 12:50:01 +0600

    tags fetcher added

--- src/components/tags/Tag.js
+++ src/components/tags/Tag.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Tag = () => {
+const Tag = ({ title }) => {
   return (
     <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
-      react
+      {title}
     </div>
   );
 };
--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchTags } from "../../features/tags/tagSlice";
 import Tag from "./Tag";
 
 const Tags = () => {
-  return (
+  const tags = useSelector((state) => state.tags.tags);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTags());
+  }, [dispatch]);
+
+  return tags.length > 0 ? (
     <section>
       <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
-        <Tag />
+        {tags.map((tag) => (
+          <Tag key={tag.id} title={tag.title} />
+        ))}
       </div>
     </section>
-  );
+  ) : null;
 };
 
 export default Tags;
