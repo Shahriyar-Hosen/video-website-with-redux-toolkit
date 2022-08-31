4bee29c961ceb2dc95a17d60cc9c697e5fa14dd4
shahriyarhosen <shahriyarhossen513@gmail.com>
2022-08-31 22:31:37 +0600

    test-1

--- src/features/videos/videosSlice.js
+++ src/features/videos/videosSlice.js
@@ -14,7 +14,6 @@ export const fetchVideos = createAsyncThunk("videos/fetchVideos", async () => {
   return videos;
 });
 
-// videos Slice
 const videosSlice = createSlice({
   name: "videos",
   initialState,
