f4ab93e4a6ce502c0b8fb97ffca41b96e0a2bbc8
shahriyarhosen <shahriyarhossen513@gmail.com>
2022-08-31 22:05:59 +0600

    tags

--- src/components/list/RelatedVideoList.js
+++ src/components/list/RelatedVideoList.js
@@ -2,6 +2,7 @@ import React from "react";
 import RelatedVideoListItem from "./RelatedVideoListItem";
 
 const RelatedVideoList = ({ tags }) => {
+  const {} = tags || {};
   return (
     <div className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
       <RelatedVideoListItem />
