9d5c949527b900aa956bca3a4e1b93228ed53ba0
shahriyarhosen <shahriyarhossen513@gmail.com>
2022-08-31 22:06:34 +0600

    Player

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -36,6 +36,7 @@ const Video = () => {
     content = (
       <div className="grid grid-cols-3 gap-2 lg:gap-8">
         <div className="col-span-full w-full space-y-8 lg:col-span-2">
+          {/* Player */}
           <VideoPlayer title={title} link={link} />
 
           <VideoDescription video={video} />
