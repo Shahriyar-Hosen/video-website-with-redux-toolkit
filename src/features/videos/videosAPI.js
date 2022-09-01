23b37f9df320ff8ae1f73bd875d8aff32d601e74
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-01 13:52:51 +0600

    get videos add query String

--- src/features/videos/videosAPI.js
+++ src/features/videos/videosAPI.js
@@ -1,6 +1,15 @@
 import axios from "../../utils/axios";
 
-export const getVideos = async () => {
-  const { data } = await axios.get("/videos");
+export const getVideos = async (tags, search) => {
+  let queryString = "";
+
+  if (tags?.length > 0) {
+    queryString += tags.map((tag) => `tags_like=${tag}`).join("&");
+  }
+  if (search !== "") {
+    queryString += `&q=${search}`;
+  }
+
+  const { data } = await axios.get(`/videos/?${queryString}`);
   return data;
 };
