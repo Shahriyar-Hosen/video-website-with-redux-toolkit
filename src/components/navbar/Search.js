f7e7d68ea03f8d303071c9903750872bc20a234a
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-01 11:25:13 +0600

    handle search filter state done

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import { searched } from "../../features/filter/filterSlice";
 
 const Search = () => {
+  const dispatch = useDispatch();
+  const { search } = useSelector((state) => state.filter);
+  
+  const [input, setInput] = useState(search);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(searched(input));
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         className="outline-none border-none mr-2"
         type="search"
         name="search"
         placeholder="Search"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
       />
     </form>
   );
