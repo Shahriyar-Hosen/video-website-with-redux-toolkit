3f59b7a08d18c75339850e3155f5c03e60d7e57e
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-02 06:10:21 +0600

    Reset function full working

--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -1,15 +1,21 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { resetFilter } from "../../features/filter/filterSlice";
 import { fetchTags } from "../../features/tags/tagSlice";
 import Tag from "./Tag";
 
 const Tags = () => {
   const tags = useSelector((state) => state.tags.tags);
+  const { tags: selectedTags, search } = useSelector((state) => state.filter);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchTags());
   }, [dispatch]);
+
+  const handleReset = () => {
+    dispatch(resetFilter());
+  };
 
   return tags.length > 0 ? (
     <section>
@@ -19,9 +25,14 @@ const Tags = () => {
             <Tag key={tag.id} title={tag.title} />
           ))}
         </div>
-        <button className="bg-cyan-200 text-cyan-700 px-4 py-1 rounded-full cursor-pointer hover:bg-cyan-300 hover:text-cyan-800">
-          Reset
-        </button>
+        {(selectedTags.length > 0 || search) && (
+          <button
+            className="bg-cyan-200 text-cyan-700 px-4 py-1 rounded-full cursor-pointer hover:bg-cyan-300 hover:text-cyan-800"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        )}
       </div>
     </section>
   ) : null;
