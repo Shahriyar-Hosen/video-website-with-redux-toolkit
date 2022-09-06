3c88b2ea6877042f305f208f2cd8493d4deff5f4
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-06 09:12:18 +0600

    index & app setup done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
 import Add from "./components/pages/Add";
@@ -6,19 +7,19 @@ import Edit from "./components/pages/Edit";
 import Home from "./components/pages/Home";
 import Video from "./components/pages/Video";
 
-function App() {
-    return (
-        <Router>
-            <Navigation />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/videos/:videoId" element={<Video />} />
-                <Route path="/videos/add" element={<Add />} />
-                <Route path="/videos/edit/:videoId" element={<Edit />} />
-            </Routes>
-            <Footer />
-        </Router>
-    );
-}
+const App = () => {
+  return (
+    <section>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos/:videoId" element={<Video />} />
+        <Route path="/videos/add" element={<Add />} />
+        <Route path="/videos/edit/:videoId" element={<Edit />} />
+      </Routes>
+      <Footer />
+    </section>
+  );
+};
 
 export default App;
