64523aa27823d75db88ec1a31e06f90f0ab0b266
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-06 09:43:39 +0600

    RTK Query Configuration ⚜ ⬇⬇⬇

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:9000",
+  }),
+  endpoints: (builder) => ({
+    
+  }),
+});
